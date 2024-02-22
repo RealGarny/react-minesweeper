3b0400efb47e13cf90ea4119c416eb32e68bd1c2
RealGarny <aseev.nik1@yandex.ru>
2024-02-22 12:13:14 +0500

    fixed leaderboard

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,27 +2,14 @@ import './Game.css';
 import MainMenu from './components/main_menu/MainMenu.jsx';
 import GameWindow from './components/game_window/GameWindow.jsx';
 import Settings from './components/settings/Settings.jsx';
-import { createLocalStorage } from '../utils/localstorage.js';
 
 import { Routes, Route} from 'react-router-dom';
 import {useSelector} from "react-redux";
 import { useDispatch } from 'react-redux';
-import { setLeadreboard } from './store/leaderboardSlice.js';
 import Leaderboard from './components/leaderboard/Leaderboard.jsx';
-import { useEffect } from 'react';
 
 function Game() {
   const gState = useSelector(state=> state.game.mState);
-  const dispatch = useDispatch();
-
-  useEffect(()=>{
-    if(!localStorage.getItem("leaderboard"))
-    {
-      createLocalStorage();
-    }else{
-      dispatch(setLeadreboard(JSON.parse(localStorage.getItem("leaderboard"))))
-    }
-  },[])
 
   return (
     <Routes>
--- src/components/leaderboard/Leaderboard.jsx
+++ src/components/leaderboard/Leaderboard.jsx
@@ -1,46 +1,63 @@
 import React from "react";
 import "./leaderboard.css";
+import { setLeadreboard } from "../../store/leaderboardSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { createLocalStorage } from "../../../utils/localstorage";
 import { useEffect, useState } from "react";
 import Button from "../UI/buttons/Button";
 import { useNavigate } from "react-router-dom";
 
 export default function Leaderboard()
 {
-    const leaderboard = useSelector(state=>state.leaderboard.leaderboard);
-    const [displayLeaderboard, setDisplayLeaderboard] = useState("easy");
+    const dispatch = useDispatch();
 
+    if(Object.keys(useSelector(state=>state.leaderboard.leaderboard)).length === 0)
+    {
+        if(!localStorage.getItem("leaderboard"))
+        {
+            createLocalStorage();
+        }
+        console.log("created")
+        dispatch(setLeadreboard(JSON.parse(localStorage.getItem("leaderboard"))));
+    }
+    
+    const leaderboard = useSelector(state=>state.leaderboard.leaderboard);
+    console.log(leaderboard)
+    const [displayLeaderboard, setDisplayLeaderboard] = useState("easy");
     const navigate = useNavigate();
+
     return(
         <div className="leaderboard">
-            <div className="h1 settings-header">
-                Leaderboard
+            <div className="leaderboard-wrapper">
+                <div className="h1 settings-header">
+                    Leaderboard
+                </div>
+                <div className="leaderboard_flex">
+                    <Button 
+                        className={`game_window-nav_button ta-c d ${displayLeaderboard==="easy" && "active"}`}
+                        action={()=>{setDisplayLeaderboard("easy")}}
+                    >easy</Button>
+                    <Button 
+                        className={`game_window-nav_button ta-c d ${displayLeaderboard==="normal" && "active"}`}
+                        action={()=>{setDisplayLeaderboard("normal")}}
+                    >normal</Button>
+                    <Button
+                        className={`game_window-nav_button ta-c d ${displayLeaderboard==="hard" && "active"}`}
+                        action={()=>{setDisplayLeaderboard("hard")}}
+                    >hard</Button>
+                </div>
+                <div className="leaderboard_content">
+                    {leaderboard[displayLeaderboard].players.map(e =>{
+                        return(
+                        <div className="leaderboard_flex">
+                            <p>nickname: {e.nickname}</p>
+                            <p>time: {e.time}s.</p>
+                        </div>
+                        )
+                    })}
+                </div>
+                <Button action={()=>{navigate("/")}}>back</Button>
             </div>
-            <div className="leaderboard_flex">
-                <Button 
-                    className={`game_window-nav_button ta-c d ${displayLeaderboard==="easy" && "active"}`}
-                    action={()=>{setDisplayLeaderboard("easy")}}
-                >easy</Button>
-                <Button 
-                    className={`game_window-nav_button ta-c d ${displayLeaderboard==="normal" && "active"}`}
-                    action={()=>{setDisplayLeaderboard("normal")}}
-                >normal</Button>
-                <Button
-                    className={`game_window-nav_button ta-c d ${displayLeaderboard==="hard" && "active"}`}
-                    action={()=>{setDisplayLeaderboard("hard")}}
-                >hard</Button>
-            </div>
-            <div className="leaderboard_content">
-                {leaderboard[displayLeaderboard].players.map(e =>{
-                    return(
-                    <div className="leaderboard_flex">
-                        <p>nickname: {e.nickname}</p>
-                        <p>time: {e.time}s.</p>
-                    </div>
-                    )
-                })}
-            </div>
-            <Button action={()=>{navigate("/")}}>back</Button>
         </div>
     )
 }
